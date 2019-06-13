1196921411d05cc21907a5149fdeb34203d030e7
Koen Klerx <0850294@hr.nl>
2019-06-14 00:50:03 +0200

    Adding Viewpage + Controller

---
+++ Blockchain/Controllers/CheckDataController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blockchain.Models;
+using Blockchain.Pages;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blockchain.Controllers
+{
+    public class CheckDataController : Controller
+    {
+        public ActionResult CheckData()
+        {
+            CheckDataModel dataBlock = new CheckDataModel
+            {
+                surname = "Rick Sanchez",
+                bsn = "None of your Business",
+                birthDate = "Some Multiverses ago",
+                type = "Wabalabadubdub",
+                value = "L..L..L..Lick mah ballzz"
+            };
+            ViewBag.Message = dataBlock;
+            return View();
+        }
+    }
+}
